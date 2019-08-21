9850d152a98384075459744bb081b246c73cb47c
Marcelo Lopes de Souza <marcelo_lopes@acad.ftec.com.br>
2019-08-20 21:47:00 -0300

    aula 5

--- src/br/com/ftec/chaves/controller/Controller.java
+++
@@ -1,5 +0,0 @@
-package br.com.ftec.chaves.controller;
-
-public class Controller {
-    
-}
---
+++ src/br/com/ftec/chaves/controller/salaDAO.java
@@ -0,0 +1,7 @@
+package br.com.ftec.chaves.controller;
+
+
+public class salaDAO {
+    String sql = "insert into sala(sala,descricao,capacidade,tipo) "
+            + "VALUES(?,?,?,?);";
+}
